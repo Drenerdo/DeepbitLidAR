b924fb3f97afbce39e3ea48c891411d12abf08c9
Dre Smith <dre@deepbitlabs.io>
2021-02-22 03:42:29 -0500

    Added world recognizer functionality to view controller script

--- DeepbitLidAR/ViewController.swift
+++ DeepbitLidAR/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
 class ViewController: UIViewController {
     
@@ -20,5 +21,22 @@ class ViewController: UIViewController {
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
+        
+        arView.environment.sceneUnderstanding.options = []
+        
+        arView.environment.sceneUnderstanding.options.insert(.occlusion)
+        
+        arView.environment.sceneUnderstanding.options.insert(.physics)
+        
+        arView.debugOptions.insert(.showSceneUnderstanding)
+        
+        arView.automaticallyConfigureSession = false
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.sceneReconstruction = .meshWithClassification
+        
+        configuration.environmentTexturing = .automatic
+        arView.session.run(configuration)
+        
+        
     }
 }
